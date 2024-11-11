42ddee5435c947873ae98827beb9ed84ab924a2a
Ahmedomar1112 <ao129875@gmail.com>
2024-11-11 20:57:46 +0200

    commit

---
+++ src/main/java/com/mycompany/matrix/Matrix.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.matrix;
+
+/**
+ *
+ * @author AhmedOmar
+ */
+public class Matrix {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
